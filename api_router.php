abd5e03b9fc9cf30b3e0df5459e96b0ef9c7c4d4
lucia adela ferraresi <luciaadelaferraresi@MacBook-Pro-de-lucia.local>
2025-11-12 08:09:46 -0300

    cambios

--- api_router.php
+++ api_router.php
@@ -9,20 +9,13 @@ require_once './app/controllers/directoresController.php';
 
 
 $router = new Router();
-$router->addRoute('auth/login', 'POST', 'AuthApiController', 'login');
-
 $router->addMiddleware(new JWTMiddleware());
-
-
+$router->addRoute('auth/login', 'POST', 'AuthApiController', 'login');
 
 $router->addRoute('peliculas', 'GET', 'peliculasController', 'getPeliculas');
 $router->addRoute('peliculas/:id', 'GET', 'peliculasController', 'getPelicula');
 $router->addRoute('peliculas/:id/resenias', 'GET', 'reseniasController', 'getResenias');
-
-
-$router->addRoute('peliculas', 'POST', 'peliculasController', 'insertPelicula');
-$router->addRoute('peliculas/:id', 'PUT', 'peliculasController', 'updatePelicula');
-$router->addRoute('peliculas/:id', 'DELETE', 'peliculasController', 'deletePelicula');
+$router->addRoute('resenias/:id', 'GET', 'reseniasController', 'getresenia');
 
 
 $router->addRoute('directores', 'GET', 'directoresController', 'getDirectores');
@@ -35,9 +28,12 @@ $router->addRoute('directores/:id', 'DELETE', 'directoresController', 'deleteDir
 
 
 $router->addMiddleware(new GuardMiddleware());
+$router->addRoute('peliculas', 'POST', 'peliculasController', 'insertPelicula');
+$router->addRoute('peliculas/:id', 'PUT', 'peliculasController', 'updatePelicula');
+$router->addRoute('peliculas/:id', 'DELETE', 'peliculasController', 'deletePelicula');
 $router->addRoute('peliculas/:id/resenias', 'POST', 'reseniasController', 'addResenia');
 $router->addRoute('resenias/:id', 'DELETE', 'reseniasController', 'deleteResenia');
 $router->addRoute('resenias/:id', 'PUT', 'reseniasController', 'updateresenia');
-$router->addRoute('resenias/:id', 'GET', 'reseniasController', 'getresenia');
+
 
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
